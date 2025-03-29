abaf26f03b6082c2777a7f97568627bd3af9731c
Érika Fernanda <e.ferlino@gmail.com>
2025-03-29 12:57:56 -0300

    Adcionando TextBlocks

--- src/JavaCore/TextBlocks.java
+++ src/JavaCore/TextBlocks.java
@@ -1,4 +1,18 @@
 package JavaCore;
 
 public class TextBlocks {
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá,\ntudo\nbem");
+
+        System.out.println("Olá,\n  tudo\n    bem");
+
+        String s = """
+                Olá
+                    Tudo
+                        Bem?""";
+
+        System.out.println(s);
+    }
 }

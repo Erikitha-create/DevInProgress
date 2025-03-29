b160745c901817c57e9dccc22f4d6c2a8238e8bc
Érika Fernanda <e.ferlino@gmail.com>
2025-03-29 15:30:05 -0300

    adcionando os operadores aritimeticos

--- src/JavaCore/OperatorArithmetic.java
+++ src/JavaCore/OperatorArithmetic.java
@@ -1,4 +1,22 @@
 package JavaCore;
 
 public class OperatorArithmetic {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = a + 5;
+        System.out.println(b);
+
+        int c = 25;
+        int d = c - b;
+        int e = a + b + d;
+        System.out.println(e);
+
+        int f = 4;
+        double g = (double) e / f;
+        double h = (double) g;
+        System.out.println(g);
+        System.out.println(h);
+    }
 }

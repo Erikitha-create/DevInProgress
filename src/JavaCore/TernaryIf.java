599587389a5626e3bac18084fc0ff35e7537ebc4
Erika Fernanda <e.ferlino@gmail.com>
2025-04-09 17:37:12 -0300

    If Ternário

---
+++ src/JavaCore/TernaryIf.java
@@ -0,0 +1,20 @@
+package JavaCore;
+
+public class TernaryIf {
+
+    public static void main(String[] args) {
+
+        int age = 19;
+//        String text;
+//
+//        if (age >= 18) {
+//            text = "Maior de idade";
+//        } else {
+//            text = "Menor de idade";
+//        }
+
+        String text = age >= 18 ? "Maior de idade" : "Menor de idade";
+
+        System.out.println(text);
+    }
+}
